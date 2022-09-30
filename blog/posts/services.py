53128c1f4df5be72af112b979cee0e462f85b98f
simplejeans <andriy.leonchik@gmail.com>
2022-09-30 12:19:08 +0300

    fixed count of like

--- blog/posts/services.py
+++ blog/posts/services.py
@@ -6,6 +6,7 @@ from posts.models import Post, Like
 def like_post(post_id, user):
     post = get_object_or_404(Post, id=post_id)
     post.likes.add(user)
+    Like.objects.get_or_create(post=post, user=user)
 
 
 def unlike_post(post_id, user):
